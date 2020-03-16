c27e65db2cbba00df909b7295f09873953d21a10
Sandesh <sandeshb981@gmail.com>
2020-03-16 20:10:40 +0100

    initial commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body class="bg-gray-200">
+<div class="flex">
+    <div class="w-1/2">
+        <img src="{{ asset('images/login/advocado-login.jpg') }}" alt="" class="object-cover h-screen w-100 overflow-hidden">
+    </div>
+    <div class="w-1/2">
+        <div class="flex justify-center h-full items-center">
+            <div class="w-full max-w-xs">
+                <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <div class="mb-2">
+                        <h1 class="text-gray-700 text-3xl text-center">Inloggen</h1>
+                    </div>
+                    <div class="mb-4">
+                        <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+                            Email
+                        </label>
+                        <input
+                            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="email"
+                            type="text"
+                            name="email"
+                            placeholder="email@domein.com">
+                    </div>
+                    <div class="mb-6">
+                        <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                            Wachtwoord
+                        </label>
+                        <input
+                            class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                            id="password"
+                            type="password"
+                            name="password"
+                            placeholder="******************">
+                        <p class="text-red-500 text-xs italic">Please choose a password.</p>
+                    </div>
+                    <div class="flex items-center justify-between">
+                        <button
+                            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                            type="submit">
+                            Sign In
+                        </button>
+                        <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+                           href="#">
+                            Forgot Password?
+                        </a>
+                    </div>
+                </form>
+                <p class="text-center text-gray-500 text-xs">
+                    &copy;2020 Acme Corp. All rights reserved.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
